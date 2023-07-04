4e30c195be6da3431747dacb21eb4aff80c5f961
Mika <mikavandenbrink@outlook.com>
2023-07-04 12:01:29 +0200

    kattijd

--- marvin.js
+++ marvin.js
@@ -1,7 +1,7 @@
 // import { Sequelize } from "./db/database";
 
 const fs = require('fs');
-const cron = require('cron');
+//const cron = require('cron');
 const Discord = require("discord.js");
 const { token } = require('./config.json');
 
@@ -31,7 +31,7 @@ for (const folder of commandFolders) {
   }
 }
 
-client.once('ready', () => {
+/*client.once('ready', () => {
   let scheduledAd = new cron.CronJob('00 30 03,09,18 * * 1-5', () => {
     const guild = client.guilds.cache.get('241646621283057665');
     const channel = guild.channels.cache.get('241646621283057665');
@@ -39,15 +39,16 @@ client.once('ready', () => {
   });
 
   scheduledAd.start();
-});
+});*/
 
 function advertentie() {
-  var random = Math.floor((Math.random()*2)+1);
+  var random = Math.floor((Math.random() * 5) + 1);
   switch (random) {
-      case 1:
-          return "DRINK VERFRISSENDE KOEKA COLA";
-      case 2:
-          return "DEZE OBER WORDT GESPONSORD DOOR REET SCHADUW LEGENDEN, DE GROOTSTE ROLLENSPEL OVER RETEN, SCHADUWEN EN LEGENDES!";
+      case 1: return "DRINK VERFRISSENDE KOEKA COLA ANDERS DRINKT HET JOU";
+      case 2: return "IS UW LEVEN COMPLEET ZONDER WENSEN, DAN IS SPECIAAL VOOR U 'SOJA VOOR WITTE MENSEN'!";
+      case 3: return "WEER EEN FAMILIETRAGEDIE? KOOP DAN NU RIJPE BRIE";
+      case 4: return "ALARM, ALARM! ER ZIJN 31 HETE, ALLEENSTAANDE MDIWN'S GEVONDEN 13KM IN DE BUURT VAN {woonplaats}. BEL SNEL NAAR 0900-MDIWN!";
+      case 5: return "DOET UW JONGEHEER HET NIET MEER? DONEER HEM DAN HET GROTE TRANS(PORT)FONDS, ZIJ VERVOEREN UW LUL NAAR EEN VOORMALIGE KNUL.";
   }
 }
 
