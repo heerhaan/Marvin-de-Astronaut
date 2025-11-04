431bd07da28d29974c3a877894d5766ed2202c4c
Tim Falken <tfalken@kvt.nl>
2025-11-04 12:38:07 +0100

    negeer emoji voor sakscontrole

--- events/message.js
+++ events/message.js
@@ -231,7 +231,8 @@ function findMatchedWords (sentence, woordenlijst)
     // normalise to lowercase for case-insensitive matching
     const lowerSentence = sentence.toLowerCase();
     const urlRegex = /\b((?:https?:\/\/|ftp:\/\/|www\.)[a-z0-9.-]+\.[a-z]{2,}(?:\/[^\s<>()]*)?)/gi;
-    sentence = sentence.replace(urlRegex, '').trim();
+    const emojiRegex = /(<a:[a-zA-Z]+:[0-9]+>)/gi
+    sentence = sentence.replace(urlRegex, '').replace(emojiRegex, '').trim();
 
     for (const item of woordenlijst)
     {
