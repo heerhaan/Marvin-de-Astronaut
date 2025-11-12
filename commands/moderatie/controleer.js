043067e2269517b40a66af915af78b51fdb8ba3a
Mika <mikavandenbrink@outlook.com>
2025-11-12 16:22:32 +0100

    Revert "dit dan"
    This reverts commit 5f6c0e162d26d425715ace5f0ba463953e8eaa94.

--- commands/moderatie/controleer.js
+++ commands/moderatie/controleer.js
@@ -1,57 +1,52 @@
 const dayjs = require('dayjs');
-const fs = require('node:fs');
 
 module.exports = {
 	name: 'controleer',
     description: 'Ter controle of er enkele spanjolen aan hun straf ontkomen zijn',
     admin: true,
 	execute(message) {
-        
-        fs.readFile('../../spanjoleringData.json', function read (err, data)
-        {
-            if (!data) return;
+        let spanjoleringen = require('../../spanjoleringData.json');
 
-            let spanjoleringen = JSON.parse(data);
+        if (!spanjoleringen) return;
 
-            let huidigeSpanjolen = [];
+        let huidigeSpanjolen = [];
 
-            for (var sleutel of Object.keys(spanjoleringen)) {
-                let manIsSpanjool = spanjoleringen[sleutel].filter(s => s.rol === "s" && s.ontjoolDatum > Date.now()).length > 0;
+        for (var sleutel of Object.keys(spanjoleringen)) {
+            let manIsSpanjool = spanjoleringen[sleutel].filter(s => s.ontjoolDatum > Date.now()).length > 0;
 
-                if (manIsSpanjool) {
-                    try {
-                        let spanjaard;
+            if (manIsSpanjool) {
+                try {
+                    let spanjaard;
 
-                        const member = message.guild.members.cache.find(member => member.id === sleutel);
+                    const member = message.guild.members.cache.find(member => member.id === sleutel);
 
-                        if (!member) break;
+                    if (!member) break;
 
-                        spanjoleringen[sleutel]
-                            .filter(s => s.ontjoolDatum > Date.now())
-                            .forEach(element => {
-                                if (!spanjaard || element.datum > spanjaard.datum) {
-                                    spanjaard = element;
-                                }
-                            });
+                    spanjoleringen[sleutel]
+                        .filter(s => s.ontjoolDatum > Date.now())
+                        .forEach(element => {
+                            if (!spanjaard || element.datum > spanjaard.datum) {
+                                spanjaard = element;
+                            }
+                        });
 
-                        huidigeSpanjolen.push(spanjaard);
-                    } catch (err) {
-                        console.error(err);
-                        return message.channel.send("auwie, dat deed pijn");
-                    }
+                    huidigeSpanjolen.push(spanjaard);
+                } catch (err) {
+                    console.error(err);
+                    return message.channel.send("auwie, dat deed pijn");
                 }
             }
+        }
 
-            if (huidigeSpanjolen.length === 0) {
-                return message.channel.send("Er zijn momenteel geen actieve spanjolen!");
-            } else {
-                for (let i = 0; i < huidigeSpanjolen.length; i++) {
-                    let sukkel = huidigeSpanjolen[i];
-                    let leesbareTijd = dayjs(sukkel.ontjoolDatum).format("DD/MM/YYYY HH:mm");
+        if (huidigeSpanjolen.length === 0) {
+            return message.channel.send("Er zijn momenteel geen actieve spanjolen!");
+        } else {
+            for (let i = 0; i < huidigeSpanjolen.length; i++) {
+                let sukkel = huidigeSpanjolen[i];
+                let leesbareTijd = dayjs(sukkel.ontjoolDatum).format("DD/MM/YYYY HH:mm");
 
-                    message.channel.send(`${sukkel.gebruikerNaam} heeft nog spanjool tot ${leesbareTijd}`);
-                }
+                message.channel.send(`${sukkel.gebruikerNaam} heeft nog spanjool tot ${leesbareTijd}`);
             }
-        });
+        }
 	},
 };
