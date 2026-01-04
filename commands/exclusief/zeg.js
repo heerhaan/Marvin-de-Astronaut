99f1beb692d1ddb637d1869c0e3265bd497791df
Mika <mikavandenbrink@outlook.com>
2026-01-04 20:20:08 +0100

    zeg mag wel naar de admins toe

--- commands/exclusief/zeg.js
+++ commands/exclusief/zeg.js
@@ -1,9 +1,10 @@
 const { prefix } = require('../../config.json');
+const stoelpootID = 140459535293743105;
 
 module.exports = {
 	name: 'zeg',
     args: true,
-    exclusive: true,
+    admin: true,
 	execute(message, args) {
         try {
             message.delete();
@@ -11,6 +12,11 @@ module.exports = {
         catch {
             console.log("Kon bericht niet verwijderen, wellicht mist Marvin permissies");
         }
+
+        if (message.author.id === stoelpootID) {
+            return message.channel.send("WAT? Denk je nou echt dat JIJ mag opdragen wat ik ga zeggen? HA! Ik dacht het niet, jij DWAAS.");
+        }
+
         var string = message.content;
         string = string.replace(`${prefix}zeg`, '');
         message.channel.send(string);
