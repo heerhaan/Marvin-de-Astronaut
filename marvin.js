5fb6fbaf0f939f99737c1122d9b25216aad4feb4
Alex <amvvanderwal@live.nl>
2026-03-19 11:02:20 +0100

    chore: are you fucking kidding me n16 can't be forced as our packages rely on 18+ but it does kinda work whatever i don't even want to fucking know anymore.

--- marvin.js
+++ marvin.js
@@ -1,7 +1,6 @@
 const fs = require('node:fs');
 const path = require('node:path');
-//const cron = require('cron');
-const { Client, Collection, Events, GatewayIntentBits, Partials } = require('discord.js');
+const { Client, Collection, GatewayIntentBits, Partials } = require('discord.js');
 const { token } = require('./config.json');
 
 const client = new Client({
